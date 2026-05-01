bcf69c1dbeeb9841b68ab349a802659ba8b9d66d
sama essam <se560192@gmail.com>
2026-05-01 17:12:43 +0300

    Remove build files from repository

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_solar_energy_FILE S:/sp project/solar_energy/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/solar_energy.exe)
-set(__QT_DEPLOY_TARGET_solar_energy_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_solar_energy_RUNTIME_DLLS C:/qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/qt/6.11.0/mingw_64/bin/Qt6Core.dll)
